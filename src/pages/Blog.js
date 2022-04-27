5539be5b5eccff2920e4c22f0a1a9c03b7964915
Sebastien CHARNET <sebastien.charnet@gmail.com>
2022-04-27 13:32:51 +0200

    :tada: Initial Commit

---
+++ src/pages/Blog.js
@@ -0,0 +1,69 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Article from "../components/Article";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const Blog = () => {
+  const [content, setContent] = useState("");
+  const [error, setError] = useState(false);
+  const [blogData, setBlogData] = useState([]);
+  const [author, setAuthor] = useState("");
+  const getData = () => {
+    axios
+      .get("http://localhost:3004/articles")
+      .then((res) => setBlogData(res.data));
+  };
+  useEffect(() => getData(), []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (content.length < 140) {
+      setError(true);
+    } else {
+      axios
+        .post("http://localhost:3004/articles", {
+          author,
+          content,
+          date: Date.now(),
+        })
+        .then(getData());
+      setError(false);
+      setAuthor("");
+      setContent("");
+    }
+  };
+
+  return (
+    <div className="blog-container">
+      <Logo />
+      <Navigation />
+      <h1>Blog</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="text"
+          placeholder="Nom"
+          onChange={(e) => setAuthor(e.target.value)}
+          value={author}
+        />
+        <textarea
+          style={{ border: error ? "1px solid red" : "1px solid grey" }}
+          placeholder="Message"
+          onChangeCapture={(e) => setContent(e.target.value)}
+          value={content}
+        />
+        {error && <p>Veuillez écrire plus de 140 caractères</p>}
+        <input type="submit" value="Envoyer" />
+      </form>
+      <ul>
+        {blogData
+          .sort((a, b) => b.date - a.date)
+          .map((article) => (
+            <Article key={article.id} article={article} />
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Blog;
